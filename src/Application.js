6302d717628b31f3e240e2043a586bf55b2c70d4
dima- <dima-tkachenko@mail.ru>
2021-01-02 00:55:27 +0300

    Разработка морской бой

---
+++ src/Application.js
@@ -0,0 +1,61 @@
+class Application {
+    mouse = null;
+
+    player = null;
+    opponent = null;
+
+    scenes = {};
+    activeScene = null;
+
+    constructor(scenes = {}) {
+        const mouse = new Mouse(document.body);
+
+        const player = new BattlefieldView(true);
+        const opponent = new BattlefieldView(false);
+
+        Object.assign(this, {mouse, player, opponent});
+
+        document.querySelector('[data-side="player"]').append(player.root);
+        document.querySelector('[data-side="opponent"]').append(opponent.root);
+
+        for (const [sceneName, SceneClass] of Object.entries(scenes)) {
+            this.scenes[sceneName] = new SceneClass(sceneName, this);
+        }
+
+        for (const scene of Object.values(this.scenes)) {
+            scene.init();
+        }
+
+        requestAnimationFrame(() => this.tick());
+    }
+
+    tick() {
+        requestAnimationFrame(() => this.tick());
+
+        if (this.activeScene) {
+            this.activeScene.update();
+        }
+
+        this.mouse.tick();
+    }
+
+    start(sceneName, ...args) {
+        if (this.activeScene && this.activeScene.name === sceneName) {
+            return false;
+        }
+
+        if (!this.scenes.hasOwnProperty(sceneName)) {
+            return false;
+        }
+
+        if (this.activeScene) {
+            this.activeScene.stop();
+        }
+
+        const scene = this.scenes[sceneName];
+        this.activeScene = scene;
+        scene.start(...args);
+
+        return true;
+    }
+}
